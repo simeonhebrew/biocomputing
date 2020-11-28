b56c685599923e9bd9a7ffcb0c393144601a938a
Ammielle WB <61096548+ammiellewb@users.noreply.github.com>
2020-11-28 14:38:58 -0500

    Update replicate_1.py

--- dna_storage/replicate_1.py
+++ dna_storage/replicate_1.py
@@ -4,13 +4,14 @@ original_str = input("What would you like to convert? ")
 # convert text to binary values
 binary_str = ''.join(format(x, '08b') for x in bytearray(original_str, 'utf-8'))
 
+binary_list = [binary_str[i: i+2] for i in range(0, len(binary_str), 2)]
+
 # binary values to nucleotide sequence
 # remember:
 # 00 = "A" (adenine)
 # 01 = "G" (guanine)
 # 10 = "C" (cytosine)
 # 11 = "T" (thymine)
-binary_list = [binary_str[i: i+2] for i in range(0, len(binary_str), 2)]
 
 DNA_encoding = {
     "00": "A",
